74b6aa0e4095c0381180f1f9a415b7a566fb5d4a
ahirner <a.hirner@gmail.com>
2019-05-11 15:49:20 +0200

    pipe_dream lib interface

---
+++ lib_pipe_dream.h
@@ -0,0 +1,4 @@
+int render_instrs(reSID::SID &sid,
+                  unsigned int *instrs, int &nr_instr,
+                  short *samples, int &nr_samples,
+                  int &idle_cycles, int &idle_samples);
